d819fc1849674b4da25205b94173e0a26cabe4ba
AllHailMegatron <ahmadrthomp@gmail.com>
2019-06-06 20:54:03 -0400

    Re-working about.html file

--- JS/about.js
+++ JS/about.js
@@ -1,16 +1,17 @@
-let row = document.querySelector(".inner");
-row = Array.from(row).forEach((inner) => {
-    new Inner(inner);
-});
 
-class Row {
-    constructor(inner) {
+// let row = document.querySelector(".inner");
+// row = Array.from(row).forEach((inner) => {
+//     new Inner(inner);
+// });
 
-    this.inner = this.domElement.querySelector(`.icons[]='${inner.dataset.tab}'`);
-    this.inner = new Inner(this.inner)
-    inner.addEventListener("click", () => this.linkClick());
-    }
-    linkClick() {
-        this.inner.toggleInner();
-    }
-}
+// class Row {
+//     constructor(inner) {
+
+//     this.inner = this.domElement.querySelector(`.icons[]='${inner.dataset.tab}'`);
+//     this.inner = new Inner(this.inner)
+//     inner.addEventListener("click", () => this.linkClick());
+//     }
+//     linkClick() {
+//         this.inner.toggleInner();
+//     }
+// }
